f249da74ac2fffd03937ef3bd50c506e18dac73b
nelsonia <dvjstaciah>
2020-07-15 15:38:40 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function getResult(){
+    var hypotenuse= parseFloat(prompt("side hypotenuse"));
+    var opposite = parseFloat(prompt("side opposite"));
+    var adjacent = parseFloat(prompt("side adjacent"));
+    
+    
+    
+    if(hypotenuse === opposite && hypotenuse === hypotenuse && opposite === adjacent){
+    alert("Equilateral");
+    }
+    else if(hypotenuse === opposite || hypotenuse === adjacent || opposite === adjacent){
+    alert("Isosceles");
+    }
+    else if((hypotenuse + opposite) <= adjacent || (hypotenuse + adjacent) <= opposite || (opposite + adjacent) <= hypotenuse) {
+    alert("Not a definite triangle");
+    }
+    else{
+    alert("Scalene");
+    }
+    }
